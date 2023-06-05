8ab106008a62f47b212331d08e37c59dcd7d9d14
kriss <kriss@xixs.com>
2023-06-05 01:31:08 +0100

    .

---
+++ js/cmd.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/*
+
+(C) Kriss@XIXs.com 2023 and released under the MIT license.
+
+See https://github.com/xriss/dilf for full notice.
+
+*/
+
+let argv=require('yargs').argv; global.argv=argv;
+let argv_parse=function(argv)
+{
+
+	//setting = command   || environment           || default
+//	argv.port = argv.port || process.env.ARSS_PORT || 12345
+
+}
+argv_parse(argv)
+
+
+
+
+
+
+
+console.log("Test");
+
