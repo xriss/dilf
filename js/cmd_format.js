81a83aaf74726f7bd342f051c317a9ca215b1121
kriss <kriss@xixs.com>
2023-06-08 12:37:33 +0100

    .

--- js/cmd_format.js
+++ js/cmd_format.js
@@ -9,6 +9,7 @@ See https://github.com/xriss/dilf for full notice.
 const cmd_format=exports;
 
 const pfs=require("fs/promises")
+const path=require("path")
 
 const mdon=require("./mdon.js")
 const jxml=require("./jxml.js")
@@ -28,35 +29,60 @@ cmd_format.run=async function(argv)
 	let ifmt="md"
 	let ofmt="json"
 	
-	if( ifname ) // input so perform conversion
+	if( ifname ) // input
 	{
 		if( ifname.endsWith(".json") ) { ifmt="json" }
 		if( ifname.endsWith(".xml") )  { ifmt="xml" }
 		if( ifname.endsWith(".md") )   { ifmt="md" }
-	
-		if( ofname ) // output
-		{
-			if( ofname.endsWith(".json") ) { ofmt="json" }
-			if( ofname.endsWith(".xml") )  { ofmt="xml" }
-			if( ofname.endsWith(".md") )   { ofmt="md" }
-		}
+	}
 
-		if( argv["json-to-xml"] ) { ifmt="json"; ofmt="xml"  }
-		if( argv["json-to-md"]  ) { ifmt="json"; ofmt="md"   }
-		if( argv["xml-to-json"] ) { ifmt="xml";  ofmt="json" }
-		if( argv["xml-to-md"]   ) { ifmt="xml";  ofmt="md"   }
-		if( argv["md-to-json"]  ) { ifmt="md";   ofmt="json" }
-		if( argv["md-to-xml"]   ) { ifmt="md";   ofmt="xml"  }
+	if( ofname ) // output
+	{
+		if( ofname.endsWith(".json") ) { ofmt="json" }
+		if( ofname.endsWith(".xml") )  { ofmt="xml" }
+		if( ofname.endsWith(".md") )   { ofmt="md" }
+	}
 
-		if( ! ofname ) // auto outname
-		{
-			if( ifname.endsWith(ifmt) ) // replace
+	if( argv["json-to-xml"] ) { ifmt="json"; ofmt="xml"  }
+	if( argv["json-to-md"]  ) { ifmt="json"; ofmt="md"   }
+	if( argv["xml-to-json"] ) { ifmt="xml";  ofmt="json" }
+	if( argv["xml-to-md"]   ) { ifmt="xml";  ofmt="md"   }
+	if( argv["md-to-json"]  ) { ifmt="md";   ofmt="json" }
+	if( argv["md-to-xml"]   ) { ifmt="md";   ofmt="xml"  }
+
+	let find_files = async function(pname,recurse,ret)
+	{
+		ret=ret || []
+		var files=await pfs.readdir(pname)
+		for(var i in files){ var v=files[i];
+			let fname=path.join(pname,v)
+			var st=await pfs.stat( fname ) // follow links
+			if( st.isDirectory() )
 			{
-				ofname=ifname.substring(0,ifname.length-ifmt.length)+ofmt
+				if(recurse)
+				{
+					await find_files(fname,recurse,ret)
+				}
+			}
+			else
+			{
+				ret.push(fname)
+			}
+		}
+		return ret
+	}
+	
+	let format=async function(ifname,ofname)
+	{
+		if(!ofname) // build output file name
+		{
+			if( ifname.endsWith("."+ifmt) ) // replace
+			{
+				ofname = ifname.substring(0,ifname.length-ifmt.length)+ofmt
 			}
 			else // add
 			{
-				ofname=ifname+"."+ofmt
+				ofname = ifname+"."+ofmt
 			}
 		}
 
@@ -65,43 +91,84 @@ Converting ${ifname} (${ifmt}) to ${ofname} (${ofmt})
 `)
 
 		let data={}
-		
-		if(ifmt=="json")
-		{
-			let text=await pfs.readFile(ifname,"utf8")
-			data = hjson.parse(text)
-		}
-		else
-		if(ifmt=="xml")
-		{
-			let text=await pfs.readFile(ifname,"utf8")
-			data = jxml.parse_xml(text)
-		}
-		else
-		if(ifmt=="md")
-		{
-			let text=await pfs.readFile(ifname,"utf8")
-			data = mdon.parse_md(text)
-		}
-
-		if(ofmt=="json")
-		{
-			let text=stringify(data,{space:" "})
-			await pfs.writeFile(ofname,text,"utf8")
-		}
-		else
-		if(ofmt=="xml")
-		{
-			let text=jxml.build_xml(data)
-			await pfs.writeFile(ofname,text,"utf8")
-		}
-		else
-		if(ofmt=="md")
-		{
-			let text=mdon.build_md(data)
-			await pfs.writeFile(ofname,text,"utf8")
+		try{
+			if(ifmt=="json")
+			{
+				let text=await pfs.readFile(ifname,"utf8")
+				data = hjson.parse(text)
+			}
+			else
+			if(ifmt=="xml")
+			{
+				let text=await pfs.readFile(ifname,"utf8")
+				data = jxml.parse_xml(text)
+			}
+			else
+			if(ifmt=="md")
+			{
+				let text=await pfs.readFile(ifname,"utf8")
+				data = mdon.parse_md(text)
+			}
+		}catch(e){
+			console.log(e)
+			await pfs.writeFile(ofname,""+e,"utf8")
+			return
 		}
 
+		try{
+			if(ofmt=="json")
+			{
+				let text=stringify(data,{space:" "})
+				await pfs.writeFile(ofname,text,"utf8")
+			}
+			else
+			if(ofmt=="xml")
+			{
+				let text=jxml.build_xml(data)
+				await pfs.writeFile(ofname,text,"utf8")
+			}
+			else
+			if(ofmt=="md")
+			{
+				let text=mdon.build_md(data)
+				await pfs.writeFile(ofname,text,"utf8")
+			}
+		}catch(e){
+			console.log(e)
+			await pfs.writeFile(ofname,""+e,"utf8")
+			return
+		}
+	}
+
+	if( argv["dirs"] ) // recursive search
+	{
+		let fnames=await find_files( argv["dirs"] , true )
+		for( let fname of fnames )
+		{
+			if( fname.endsWith("."+ifmt) ) // found
+			{
+				await format(fname)
+			}
+		}
+		return
+	}
+	else
+	if( argv["dir"] ) // single dir search
+	{
+		let fnames=await find_files( argv["dir"] , false )
+		for( let fname of fnames )
+		{
+			if( fname.endsWith("."+ifmt) ) // found
+			{
+				await format(fname)
+			}
+		}
+		return
+	}
+	else
+	if( ifname ) // input file
+	{
+		await format(ifname,ofname)
 		return
 	}
 
@@ -112,7 +179,8 @@ Converting ${ifname} (${ifmt}) to ${ofname} (${ofmt})
 
 We try and auto pick the input and output formats from the filenames 
 extensions but you can also force specific formats using the following 
-flags.
+flags. Note expect lowercase and if we can not guess the format we 
+deafult to --md-to-json so will assume these formats.
 
 >	dilf format --json-to-xml infilename.json [outfilename.xml]
 >	dilf format --json-to-md infilename.json [outfilename.md]
@@ -122,6 +190,18 @@ flags.
 
 >	dilf format --xml-to-json infilename.xml [outfilename.json]
 >	dilf format --xml-to-md infilename.xml [outfilename.md]
+
+
+>	dilf format --md-to-xml --dirs=.
+
+Convert an entire directory, recuresivly, any .md files found will be 
+converted to a .xml This depensds of course on your choice of formats.
+
+>	dilf format --md-to-xml --dir=.
+
+Note the use of --dir instead of --dirs , this stops recursion into sub 
+directories so only the directory explicitly given will be searched for 
+.md files.
 
 `)
 
